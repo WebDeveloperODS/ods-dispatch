3d88216bc8885e630685178052675938eaea479c
WebDeveloperODS <webdeveloper.ods@gmail.com>
2025-10-08 23:03:52 +0500

    Added vite-ssg for ssr seo

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.jsx'
+import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+export const createApp = ViteSSG(
+  () => (
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  ),
+  {
+    routes: [
+      { path: '/' },
+      { path: '/about-ods-dispatch' },
+      { path: '/truck-types' },
+      { path: '/services-by-ods/dispatch-and-brokerage' },
+      { path: '/services-by-ods/social-media-management' },
+      { path: '/services-by-ods/business-web-development' },
+      { path: '/carriers-setup' },
+      { path: '/to-contact-ods-dispatch' },
+    ],
+  }
 )
